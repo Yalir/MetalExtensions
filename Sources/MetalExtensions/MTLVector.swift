b2a5b7b595e04bc76d3cca695d79146a8f96c93e
Ceylo <lucas.soltic@orange.fr>
2019-11-03 12:32:35 +0100

    Fix visibility

--- Sources/MetalExtensions/MTLVector.swift
+++ Sources/MetalExtensions/MTLVector.swift
@@ -9,24 +9,24 @@
 import Foundation
 
 public struct MTLVector {
-    var dx: Int = 0
-    var dy: Int = 0
-    var dz: Int = 0
+    public var dx: Int = 0
+    public var dy: Int = 0
+    public var dz: Int = 0
     
-    init(dx: Int = 0, dy: Int = 0, dz: Int = 0) {
+    public init(dx: Int = 0, dy: Int = 0, dz: Int = 0) {
         self.dx = dx
         self.dy = dy
         self.dz = dz
     }
     
-    init(vector: CGVector, residual: inout CGVector) {
+    public init(vector: CGVector, residual: inout CGVector) {
         dx = Int(vector.dx)
         dy = Int(vector.dy)
         
         residual = CGVector(dx: vector.dx - CGFloat(dx), dy: vector.dy - CGFloat(dy))
     }
     
-    var isNull: Bool {
+    public var isNull: Bool {
         dx == 0 && dy == 0 && dz == 0
     }
 }
