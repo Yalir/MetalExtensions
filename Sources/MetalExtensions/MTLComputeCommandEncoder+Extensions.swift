4d6a0c56b245bbd375d22bb5fdd65c38db938e7c
Lucas Soltic <lucas.soltic@orange.fr>
2020-09-06 13:14:59 +0200

    WIP Customize dispatch, looks great!

--- Sources/MetalExtensions/MTLComputeCommandEncoder+Extensions.swift
+++ Sources/MetalExtensions/MTLComputeCommandEncoder+Extensions.swift
@@ -9,13 +9,13 @@
 import Metal
 
 public extension MTLComputeCommandEncoder {
-    func dispatchThreadgroupsForWorkingOn(_ texture: MTLTexture) {
+    func dispatchThreadgroupsForWorkingOn(_ texture: MTLTexture, threadExecutionWidth: Int = 16) {
         let size = MTLSize(width: texture.width, height: texture.height, depth: 1)
-        dispatchThreadgroupsForWorkingOn(size)
+        dispatchThreadgroupsForWorkingOn(size, threadExecutionWidth: threadExecutionWidth)
     }
     
-    func dispatchThreadgroupsForWorkingOn(_ size: MTLSize) {
-        let threadGroupSize = MTLSize(width: 16, height: 16, depth: 1)
+    func dispatchThreadgroupsForWorkingOn(_ size: MTLSize, threadExecutionWidth: Int = 16) {
+        let threadGroupSize = MTLSize(width: 1, height: threadExecutionWidth, depth: 1)
         let threadGroupCount = MTLSize(
             width: (size.width + threadGroupSize.width - 1) / threadGroupSize.width,
             height: (size.height + threadGroupSize.height - 1) / threadGroupSize.height,
